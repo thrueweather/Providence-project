14d766bfcaa56aa33774de3a7f26ca623c503419
dmitriypopov1512 <dmitriypopov151298@gmail.com>
2017-10-23 13:35:37 +0300

    Added script to navigation

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
-
-	// Scrolling
+// Scrolling
 $('a[data-target^="anchor"]').bind('click.smoothscroll', function(){
 		var target = $(this).attr('href'),
 			bl_top = $(target).offset().top - 70;
 		$('body, html').animate({scrollTop: bl_top}, 2000);
 		return false;
 	});
+
+
+// МЕНЮ 
+$(function(){
+    $(window).scroll(function() {
+        if ($(this).scrollTop() >= 1) {
+            $('.main-nav').addClass('stickytop');
+        }
+        else {
+            $('.main-nav').removeClass('stickytop');
+        }
+    });
+});
